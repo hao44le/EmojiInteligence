ec3444cc99e51ba9ce21281b053c652a417f1a51
Grey Chen <957024640@qq.com>
2019-04-21 10:16:29 +0800

    Initial Commit

---
+++ EmojiInteligence/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiInteligence
+//
+//  Created by Gelei Chen on 2019/4/21.
+//  Copyright © 2019 Gelei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
