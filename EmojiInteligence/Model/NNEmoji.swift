88bd91bb0c07ed01a18ed26858a7163ce713cf9c
Grey Chen <957024640@qq.com>
2019-04-21 10:48:33 +0800

    copy from storyboard to xcode

---
+++ EmojiInteligence/Model/NNEmoji.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+                // Emoji 表情
+public struct NNEmoji {
+    /// The Emoji Label 标签标签
+    let emoji: String
+    /// The explaination text for the drawing 表情上面的文本
+    let drawText: String
+    /// The Button Text 按钮上面的文本
+    let buttonText: String
+}
